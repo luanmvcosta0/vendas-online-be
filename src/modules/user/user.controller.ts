6d15490df4665dc98d587dbd5a1844c822561392
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-11 10:46:07 -0300

    feat: create a createUser

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Post()
+  createUser(@Body() dto: CreateUserDto) {
+    return {
+      ...dto,
+      password: undefined,
+    };
+  }
+}
