009d0b324f025d779668462e879d11c9f035b48d
luanmvcosta0 <luanmvcosta@gmail.com>
2026-05-11 14:18:13 -0300

    feat: implement user creation with password hashing and add bcrypt dependency

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  createUser(@Body() dto: CreateUserDto) {
-    return {
-      ...dto,
-      password: undefined,
-    };
+  async createUser(@Body() dto: CreateUserDto): Promise<User> {
+    return this.userService.createUser(dto);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './entities/user.entity';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  private users: User[] = [];
+
+  async createUser(dto: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+
+    const passwordHashed = await hash(dto.password, saltOrRounds);
+
+    const user: User = {
+      ...dto,
+      id: this.users.length + 1,
+      password: passwordHashed,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+}
